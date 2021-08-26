69f74f078a552f0037ad044fde8bf1b57ed138d8
Megane-chyan <absurd769@gmail.com>
2021-08-26 21:51:42 +0300

    add positive tests

--- src/test/java/NegativeTest.java
+++ src/test/java/NegativeTest.java
@@ -8,19 +8,36 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class NegativeTest extends BaseTest{
 
+    //TODO доделать негативные тесты
     @ParameterizedTest
     @NullSource
-    @ValueSource(strings = {"999999999999999999999", "0", "3.0", "test", "-1", "01", "2 5", " 5", "9 ",
-    "-999999999999999999999"})
-    void getRegionsPageNegativeTest(String i){
-
+    @ValueSource(strings = {"ТОМСК", "moscow", "тест", "Ош", " ", "~!@#$%^&*()?>,./",
+            "Уилла Реал де ла Санта-Фе де Сан-Франциско де Асис"})
+    void getNameRegionSearchNegativeTest(String i){
         Unirest.get("")
                 .queryString("page", i)
                 .asJson()
                 .ifSuccess(jsonResponse -> assertThat(jsonResponse.getStatus(), equalTo(200)))
+                .ifFailure(jsonResponse -> {logger.error(jsonResponse.getStatus());
+                    jsonResponse.getParsingError().ifPresent(e -> logger.error("Error body: " + e.getOriginalBody()));});
+    }
+
+    //TODO разделить обрабатываемые ошибки и обычные запросы
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"999999999999999999999", "0", "3.0", "test", "-1", "01", "2 5", " 5", "9 ",
+    "-999999999999999999999", " "})
+    void getRegionsPageNegativeTest(String i){
+        Unirest.get("")
+                .queryString("page", i)
+                .asJson()
+                .ifSuccess(jsonResponse -> {
+                    assertThat(jsonResponse.getStatus(), equalTo(200));
+                    assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));})
                 .ifFailure(jsonResponse -> {logger.error(jsonResponse.getStatus());
                 jsonResponse.getParsingError().ifPresent(e -> logger.error("Error body: " + e.getOriginalBody()));});
     }
--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -1,15 +1,13 @@
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
-import org.hamcrest.CoreMatchers;
-import org.junit.jupiter.api.DisplayName;
+import kong.unirest.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class PositiveTest extends BaseTest{
     @Test
@@ -17,21 +15,24 @@ public class PositiveTest extends BaseTest{
         HttpResponse<JsonNode> response = Unirest.get("")
                 .asJson();
         int total = response.getBody().getObject().getInt("total");
-        //int s = response.getBody().getObject().getJSONArray("items").length(); использовать для подсчета page size
-       // assertThat(response.getBody().getArray(), CoreMatchers.is(notNullValue()));
+        int s = response.getBody().getObject().getJSONArray("items").length();
+       //assertThat(response.getBody().getArray(), CoreMatchers.is(notNullValue()));
+        System.out.println(s);
         assertThat(response.getStatus(), equalTo(200));
         assertThat(total, equalTo(22));
     }
 
+    //TODO проверка response пустой или нет?
     @ParameterizedTest
-    @ValueSource(strings = {"влад", "Актобе", "Уфа", "Омск", "бирск", "москва"})
-    void getCodeRegionSearchPositiveTest(String i){
+    @ValueSource(strings = {"влад", "Актобе", "Уфа", "Омск", "бирск", "москва",
+            "Усть-Каменогорск","Нижний Новгород"})
+    void getNameRegionSearchPositiveTest(String i){
         Unirest.get("")
                 .queryString("q", i)
                 .asJson()
                 .ifSuccess(jsonResponse -> assertThat(jsonResponse.getStatus(), equalTo(200)))
                 .ifFailure(jsonResponse -> {
-                    System.out.println(jsonResponse.getStatus());
+                    logger.error(jsonResponse.getStatus());
                 });
     }
 
@@ -43,7 +44,38 @@ public class PositiveTest extends BaseTest{
                 .asJson()
                 .ifSuccess(jsonResponse -> assertThat(jsonResponse.getStatus(), equalTo(200)))
                 .ifFailure(jsonResponse -> {
-                    System.out.println(jsonResponse.getStatus());
+                    logger.error(jsonResponse.getStatus());
+                });
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 15})
+    void getRegionsPageSizePositiveTest(int i){
+        Unirest.get("")
+                .queryString("page_size", i)
+                .asJson()
+                .ifSuccess(jsonResponse -> {assertThat(jsonResponse.getStatus(), equalTo(200));
+                assertThat(jsonResponse.getBody().getObject().getJSONArray("items").length(), equalTo(i));})
+                .ifFailure(jsonResponse -> {
+                    logger.error(jsonResponse.getStatus());
+                });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ru", "kg", "kz", "cz"})
+    void getRegionsCountryCodePositiveTest(String i){
+        Unirest.get("")
+                .queryString("country_code", i)
+                .asJson()
+                .ifSuccess(jsonResponse -> {assertThat(jsonResponse.getStatus(), equalTo(200));
+                    JSONArray array = jsonResponse.getBody().getObject().getJSONArray("items");
+                    for(int j=0;j<array.length();j++){
+                        String object = array.getJSONObject(j).getJSONObject("country").getString("code");
+                        assertThat(object,equalTo(i));
+                    }
+                })
+                .ifFailure(jsonResponse -> {
+                    logger.error(jsonResponse.getStatus());
                 });
     }
 }
