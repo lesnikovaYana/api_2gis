841d533834ed897dc33c6efcaddad6923d4229d7
Megane-chyan <absurd769@gmail.com>
2021-08-27 20:59:04 +0300

    add negative tests

--- src/test/java/NegativeTest.java
+++ src/test/java/NegativeTest.java
@@ -1,6 +1,7 @@
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -19,9 +20,15 @@ public class NegativeTest extends BaseTest{
             "Уилла Реал де ла Санта-Фе де Сан-Франциско де Асис"})
     void getNameRegionSearchNegativeTest(String i){
         Unirest.get("")
-                .queryString("page", i)
+                .queryString("q", i)
                 .asJson()
-                .ifSuccess(jsonResponse -> assertThat(jsonResponse.getStatus(), equalTo(200)))
+                .ifSuccess(jsonResponse -> {
+                        assertThat(jsonResponse.getStatus(), equalTo(200));
+                        //assertThat(jsonResponse.getBody().getArray(), CoreMatchers.is(notNullValue()));
+                        //JSONArray array = jsonResponse.getBody().getObject().getJSONArray("items");
+                        //assertThat(array.length(),CoreMatchers.is(notNullValue()));
+                        assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));
+                })
                 .ifFailure(jsonResponse -> {logger.error(jsonResponse.getStatus());
                     jsonResponse.getParsingError().ifPresent(e -> logger.error("Error body: " + e.getOriginalBody()));});
     }
@@ -37,8 +44,42 @@ public class NegativeTest extends BaseTest{
                 .asJson()
                 .ifSuccess(jsonResponse -> {
                     assertThat(jsonResponse.getStatus(), equalTo(200));
-                    assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));})
+                    assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));
+                })
                 .ifFailure(jsonResponse -> {logger.error(jsonResponse.getStatus());
                 jsonResponse.getParsingError().ifPresent(e -> logger.error("Error body: " + e.getOriginalBody()));});
+    }
+
+    //TODO попробовать через for и try catch
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"999999999999999999999", "0", "3.0", "test", "-1", "01", "1 5", " 5", "10 ",
+            "-999999999999999999999", " ", "16"})
+    void getRegionsPageSizeNegativeTest(String i){
+        Unirest.get("")
+                .queryString("page_size", i)
+                .asJson()
+                .ifSuccess(jsonResponse -> {
+                     assertThat(jsonResponse.getStatus(), equalTo(200));
+                     assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));})
+                .ifFailure(jsonResponse -> {
+                    logger.error(jsonResponse.getStatus());
+                });
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"ua", "RU", "тест", "ca", "y", " ", "~!@#$%^&*()?>,./"})
+    void getRegionsCountryCodeNegativeTest(String i) {
+        Unirest.get("")
+                .queryString("country_code", i)
+                .asJson()
+                .ifSuccess(jsonResponse -> {
+                    assertThat(jsonResponse.getStatus(), equalTo(200));
+                    assertThat(jsonResponse.getBody().getObject().get("error"), CoreMatchers.is(notNullValue()));
+                })
+                .ifFailure(jsonResponse -> {
+                    logger.error(jsonResponse.getStatus());
+                });
     }
 }
